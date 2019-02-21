98307e096f9d35218aca3fa5123f71d69c819d38
Peng Chong <1551127928@qq.com>
2019-02-21 11:24:17 +0800

    Fix bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-
+import LoginComponent from "./views/login.vue";
+import FirstComponent from "./views/first_page.vue";
 
 Vue.use(VueRouter);
 
