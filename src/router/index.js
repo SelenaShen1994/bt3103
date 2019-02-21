10f9a40b0402aa6b52f8089817853b0f2e995de9
Peng Chong <1551127928@qq.com>
2019-02-21 11:33:28 +0800

    Add four_year_plan.vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import LoginComponent from "@/views/login.vue";
 import FirstComponent from "@/views/first_page.vue";
+import PlanComponent from "@/compnents/four_year_plan.vue"
 
 Vue.use(VueRouter);
 
@@ -22,6 +23,11 @@ export default new VueRouter({
         path: "/first_page",
         name: "first_page",
         component: FirstComponent
+      },
+      {
+        path:"/four_year_plan",
+        name: "four_year_plan",
+        component: PlanComponent
       }
     ]
   });
